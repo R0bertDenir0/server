1d88ddba4c7d490a67e6bc17b0b85b266025697a
R0bertDenir0 <luka.michelson@gmail.com>
2022-11-21 15:45:59 +0100

    working TCP socket

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use std::io::Read;
 
 pub struct Server {
         addr: String,
@@ -15,7 +16,29 @@ pub struct Server {
     
             println!("server running on port {}", self.addr);
             
-            let listener = TcpListener::bind(&self.addr);
+            let listener = TcpListener::bind(&self.addr).unwrap();
+
+            loop {
+                match listener.accept(){
+                    Ok((mut stream, _)) => {
+                        let mut buffer = [0; 1024];
+                       
+                        match stream.read(&mut buffer) {
+                            Ok(_) => {
+                                println!("Received a request : {}", String::from_utf8_lossy(&buffer));
+                            },
+                            Err(e) => println!("failed to connect  {}", e),
+
+                        }
+                        println!("OK!")
+                    }
+                    Err(e) => println!("failed to established a connection : {}", e),
+                }
+            }
+
+        
+
+            
     
         }
 
